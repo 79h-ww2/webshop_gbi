f983cb8705db1ca4d21797968dd0a228385dfd4b
benedikt <benedikt@benedikt-Aspire-M5811>
2015-05-22 00:18:04 +0200

    erster Entwurf Suchformular in Header

--- webshop_gbi/index.php
+++ webshop_gbi/index.php
@@ -42,7 +42,7 @@
             </div>
             
             <div class="searchCell">
-            
+            	<?php include_once 'Pages/suche_header.php.inc'; ?>
             </div>
             
             <div class="warenkorbCell">
