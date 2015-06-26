fb77fa61738dabf060288ffc295b196091f02d28
benedikt <benedikt@benedikt-Notebook>
2015-06-26 16:49:05 +0200

    Fehlermeldung JS ungültige Eingabe Warenkorb

--- webshop_gbi/Funktions/JS/vorcheck_warenkorb.js
+++ webshop_gbi/Funktions/JS/vorcheck_warenkorb.js
@@ -11,5 +11,25 @@ function eintrag_in_warenkorb_vorcheck(){
 	//überprüft, ob alle Felder ausgefüllt sind
 	fehlermeldung = sindAlleFelderAusgefuellt(felder, meldungswort, 'from_warenkorb');
 	
+	if (fehlermeldung == ''){
+		if (isNumeric(document.forms['from_warenkorb']['menge'].value) == false){
+			fehlermeldung += fehlermeldung == '' ? '' : ' ';
+			fehlermeldung += 'Die Mege muss ein Zahlwert sein.';
+			
+			//Textbox rot umranden
+			document.forms['from_warenkorb']['menge'].style.borderColor = 'red';
+			document.forms['from_warenkorb']['menge'].style.borderStyle = 'solid';
+		}
+		else if (document.forms['from_warenkorb']['menge'].value < 1){
+			fehlermeldung += fehlermeldung == '' ? '' : ' ';
+			fehlermeldung += 'Die Menge muss eine positive Zahl sein.';
+		}
+	}
 	
+	//zeigt die Fehlermeldung auf der Webseite an
+	if (fehlermeldung != ''){
+		document.getElementById('fehlermeldung_warenkorb').innerHTML = fehlermeldung;
+	}
+	
+	return fehlermeldung == ''; 
 }
