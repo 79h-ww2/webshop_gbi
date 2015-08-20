e2a6aef5a7c77def60ffeb0e13f180d06f3cf011
benedikt <benedikt@benedikt-Aspire-M5811>
2015-08-20 22:54:19 +0200

    Quelltext gekürzt

--- webshop_gbi/Funktions/JS/anmeldung.js
+++ webshop_gbi/Funktions/JS/anmeldung.js
@@ -16,44 +16,10 @@ function vorcheckEingabe_anmeldung(formularName){
 	//Array mit Meldungswörtern, die in der Fehlermeldung angezeigt werden sollen
 	var meldungswort = ['ihre Email-Adresse', 'ihr Passwort'];
 	
-	//Array, wo alle nicht ausgefüllten Felder gespeichert werden sollen
-	var nicht_ausgefuellte_felder = [];
-	
-	//überprüft jedes Feld
-	for ( var i = 0; i < felder.length; i++){
-		if  ( document.forms[formularName][felder[i]].value == ''){
-			nicht_ausgefuellte_felder.push(meldungswort[i]);
-			
-			//Textbox rot umranden
-			document.forms[formularName][felder[i]].style.borderColor = 'red';
-			document.forms[formularName][felder[i]].style.borderStyle = 'solid';
-		}
-		else{
-			//alte Textboxumrandung löschen, falls der Fehler behoben wurde
-			document.forms[formularName][felder[i]].style.borderColor = '';
-			document.forms[formularName][felder[i]].style.borderStyle = '';
-		}
-	}
-	
-	//wenn nicht jedes Feld ausgefüllt ist, wird eine Fehlermeldung generiert
-	if (nicht_ausgefuellte_felder.length > 0){
-		if ( nicht_ausgefuellte_felder.length == 1){
-			fehlermeldung = 'Bitte geben Sie ' +  nicht_ausgefuellte_felder[0] + ' an.';
-		}else{
-			for (var j = 0; j < nicht_ausgefuellte_felder.length; j++){
-				if (j == 0){
-					fehlermeldung = 'Bitte geben Sie ' +  nicht_ausgefuellte_felder[j];
-				}else if ( j < (nicht_ausgefuellte_felder.length -1)){
-					fehlermeldung += ', ' + nicht_ausgefuellte_felder[j];
-				}else{
-					fehlermeldung += ' und ' + nicht_ausgefuellte_felder[j] + ' an.';
-				}
-			}
-		}
-	}
-	//wenn alle Felder ausgefüllt sind, wird überprüft, ob die Eingaben gültig sind
-	else{
+	fehlermeldung = sindAlleFelderAusgefuellt(felder, meldungswort, formularName);
 		
+	
+	if (fehlermeldung == ''){
 		//überprüft, ob die Email-Adresse gültig ist
 		if ( checkEmail(document.forms[formularName]['anmedlung_email'].value) == false ){
 			fehlermeldung += fehlermeldung == '' ? '' : ' ';
@@ -74,17 +40,4 @@ function vorcheckEingabe_anmeldung(formularName){
 	
 	//übergibt den Rückgabewert "true", wenn kein Fehler aufgetreten ist. Ist Fehler aufgetreten wird ein "false" ausgegeben
 	return fehlermeldung == ''; 
-}
-
-
-
-/**
- * die Email-Adresse prüfen
- * Quelle: http://www.w3resource.com/javascript/form/email-validation.php
- * @param {String} email Die Email-Adresse, die überprüft werden soll
- * @returns {Boolean} gibt aus, ob die Email-Adresse gültig ist 
- */
-function checkEmail(email)
-{
-	return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
 }
