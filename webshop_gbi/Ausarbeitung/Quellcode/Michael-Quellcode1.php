3f2a1674d62c5da7f7cdeccf4a47f39ea6cb7611
Stefan <Stefan@192.26.177.27>
2015-09-05 11:04:57 +0200

    Ausarbeitung-Teil von Michael eingefügt

---
+++ webshop_gbi/Ausarbeitung/Quellcode/Michael-Quellcode1.php
@@ -0,0 +1,24 @@
+<?php
+				
+function check_mobile() {
+$agents = array('Windows CE', 'Pocket', 'Mobile',
+			   'Portable', 'Smartphone', 'SDA',
+			   'PDA', 'Handheld', 'Symbian',
+			   'WAP', 'Palm', 'Avantgo',
+			   'cHTML', 'BlackBerry', 'Opera Mini',
+			   'Nokia');
+	
+// Pruefen der Browserkennung
+	for ($i=0; $i<count($agents); $i++) {
+if(isset($_SERVER["HTTP_USER_AGENT"]) && strpos($_SERVER["HTTP_USER_AGENT"], $agents[$i]) !== false)
+			return true;
+		}	 
+		return false;
+	}
+if (check_mobile()) {
+	?>
+<meta name="viewport" content="width=device-width, minimum-scale=2, initial-scale=1.0, user-scalable=no">
+	<link rel="stylesheet" href="Styles/mobile.css" type="text/css"/>
+	<?php
+}	 
+?>
